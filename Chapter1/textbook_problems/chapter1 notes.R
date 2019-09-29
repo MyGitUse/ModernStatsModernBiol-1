9f60fe790b3dba8b7c4973948a32d091e461aa4b
Samantha Lee <samantha.lee447@gmail.com>
2019-09-29 15:57:57 -0500

    commit from mac

---
+++ Chapter1/textbook_problems/chapter1 notes.R
@@ -0,0 +1,12 @@
+#sept 25, 2019
+
+#think about what you want for final project
+#do not do same data type as thesis
+
+#goals
+
+#add in information
+
+#binomial is step above bernoulli (2 outcomes)
+
+#monte carlo for thesis (can randomize data so not having to justify using parametric or nonparametric test)
